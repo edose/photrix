f76ef687c8f202248a3fe5df9b08e05376f39679
Eric Dose <astro@ericdose.com>
2020-02-19 10:07:17 -0700

    roof.py: minor improvements (DSW)

--- photrix/roof.py
+++ photrix/roof.py
@@ -12,15 +12,16 @@ from util import hhmm_from_datetime_utc
 STATUS_URL = 'https://beta.deepskywest.com/status.php'
 HTTP_OK_CODE = 200  # "OK. The request has succeeded."
 MAX_CONSECUTIVE_TIMEOUTS = 60
-SECONDS_BETWEEN_QUERIES = 60
+SECONDS_BETWEEN_STARTUP_QUERIES = 5
+SECONDS_BETWEEN_ONGOING_QUERIES = 60
 QUERIES_REQUIRED = 1
 TAG_TO_TEST = 'beta roof status'
 CLOSED_TAGS = ['closing', 'closed']
 OPENED_TAGS = ['opening', 'open']
 # PATTERN_HAS_OPENED = 1 * ['closed'] + QUERIES_REQUIRED * ['open']
 # PATTERN_HAS_CLOSED = 1 * ['open'] + QUERIES_REQUIRED * ['closed']
-SOUND_HAS_OPENED = 'SystemAsterisk'
-SOUND_HAS_CLOSED = 'SystemHand'
+SOUND_HAS_OPENED = 'SystemAsterisk'  # = Win 10 Asterisk; use Sonata/Windows Error.wav
+SOUND_HAS_CLOSED = 'SystemHand'      # = Win 10 Critical Stop; use Sonata/Windows Critical Stop.wav
 SOUND_REPETITIONS = 100
 
 
@@ -35,7 +36,7 @@ def trial():
     status_list = (QUERIES_REQUIRED + 1) * ['-']
     n_timeouts = 0
     while True:
-        r = None  # keep IDE happy
+        r = None  # (initialize to keep IDE happy)
         try:
             r = requests.get(STATUS_URL)
         except requests.exceptions.Timeout:
@@ -70,17 +71,17 @@ def trial():
             print(' >>>>> OPENED at', hhmm)
             for i in range(SOUND_REPETITIONS):
                 winsound.PlaySound(SOUND_HAS_OPENED, winsound.SND_ALIAS)
-            return
+            # return  # let's try leaving it running for further notifications (20200219).
         elif has_closed(status_list):
             print(' >>>>> CLOSED at', hhmm)
             for i in range(SOUND_REPETITIONS):
                 winsound.PlaySound(SOUND_HAS_CLOSED, winsound.SND_ALIAS)
-            return
+            # return  # let's try leaving it running for further notifications (20200219).
         else:
             if any([status == '-' for status in status_list]):
-                sleep(5)  # only for starting up.
+                sleep(SECONDS_BETWEEN_STARTUP_QUERIES)
             else:
-                sleep(SECONDS_BETWEEN_QUERIES)
+                sleep(SECONDS_BETWEEN_ONGOING_QUERIES)
 
 
 def has_opened(status_list):
